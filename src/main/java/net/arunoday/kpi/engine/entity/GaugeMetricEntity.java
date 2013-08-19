11af3c8001b549cfc22c86951a661ec99b6e9566
Aparna Chaudhary <aparna.chaudhary@gmail.com>
2013-08-19 22:08:38 +0200

    #7 Added Entity for Gauge Metric

---
+++ src/main/java/net/arunoday/kpi/engine/entity/GaugeMetricEntity.java
@@ -0,0 +1,101 @@
+/**
+ * 
+ */
+package net.arunoday.kpi.engine.entity;
+
+import java.util.Date;
+
+/**
+ * Represents the Metric information for Gauge events.
+ * 
+ * @author Aparna Chaudhary
+ * 
+ */
+public class GaugeMetricEntity {
+
+	/** unique identifier for this event */
+	private String id;
+	/** Type of event. */
+	private String eventType;
+	/** Resolution of metric */
+	private String resolution;
+	/** Time of occurrence of event */
+	private Date timestamp;
+	private String operation;
+	private double value;
+
+	/**
+	 * Public constructor
+	 */
+	public GaugeMetricEntity() {
+		super();
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public String getEventType() {
+		return eventType;
+	}
+
+	public void setEventType(String eventType) {
+		this.eventType = eventType;
+	}
+
+	public MetricResolution getResolution() {
+		return MetricResolution.fromCode(resolution);
+	}
+
+	public void setResolution(MetricResolution resolution) {
+		this.resolution = resolution.getCode();
+	}
+
+	public Date getTimestamp() {
+		return timestamp;
+	}
+
+	public void setTimestamp(Date timestamp) {
+		this.timestamp = timestamp;
+	}
+
+	public String getOperation() {
+		return operation;
+	}
+
+	public void setOperation(String operation) {
+		this.operation = operation;
+	}
+
+	public double getValue() {
+		return value;
+	}
+
+	public void setValue(double value) {
+		this.value = value;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null || !getClass().equals(obj.getClass())) {
+			return false;
+		}
+		GaugeMetricEntity that = (GaugeMetricEntity) obj;
+		return this.getId().equals(that.getId());
+	}
+
+	@Override
+	public int hashCode() {
+		return getId().hashCode();
+	}
+
+	@Override
+	public String toString() {
+		return "GaugeMetricEntity [id=" + id + ", eventType=" + eventType + ", resolution=" + resolution + ", timestamp="
+				+ timestamp + ", operation=" + operation + ", value=" + value + "]";
+	}
+
+}
---
+++ src/main/java/net/arunoday/kpi/engine/entity/MetricResolution.java
@@ -0,0 +1,50 @@
+package net.arunoday.kpi.engine.entity;
+
+/**
+ * Resolution for Metric data
+ * 
+ * @author Aparna
+ * 
+ */
+public enum MetricResolution {
+
+	YEAR("y"), MONTH("m"), DAY("d"), HOUR("h"), MINUTE("m");
+
+	private String code;
+
+	private MetricResolution(String code) {
+		this.code = code;
+	}
+
+	/**
+	 * Returns resolution code
+	 * 
+	 * @return metric resolution code
+	 */
+	public String getCode() {
+		return code;
+	}
+	
+	/**
+	 * Constructs {@link MetricResolution} based on code.
+	 * 
+	 * @param code resolution code
+	 * @return {@link MetricResolution} for the code; {@literal null} if none found.
+	 */
+	public static MetricResolution fromCode(String code) {
+		if (code != null) {
+			for (MetricResolution metric : MetricResolution.values()) {
+				if (code.equalsIgnoreCase(metric.code)) {
+					return metric;
+				}
+			}
+		}
+		return null;
+	}
+
+	@Override
+	public String toString() {
+		return code;
+	}
+
+}
